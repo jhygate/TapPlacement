f227c5f09f8d34b605a7bb48de09926fefe0c84c
Jack Hygate <jhygate@gmail.com>
2019-02-17 02:53:42 +0000

    Removed GreedyBrute bug

--- resize.py
+++ resize.py
@@ -3,7 +3,7 @@ import cv2
 img = cv2.imread("village.png")
 height, width, channels = img.shape
 print(height,width)
-scale = 1
+scale = 300/(max(height,width))
 small = cv2.resize(img, (0,0), fx=scale, fy=scale)
 
 cv2.imwrite('resized.jpg',small)
