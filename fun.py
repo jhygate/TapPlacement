30fcd9dedc7debaccfbe7cf6a74f4d7f2763db25
Matthew Jones <matthewbtjones2000@gmail.com>
2019-02-17 05:34:36 +0000

    Tidied and added progress bar

--- fun.py
+++
@@ -1,9 +0,0 @@
-import folium
-
-#token = "pk...." # your mapbox token
-#tileurl = 'https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}@2x.png?access_token=' + str(token)
-
-m = folium.Map(
-    location=[48.73596, 11.18434], zoom_start=9)#, tiles=tileurl, attr='Mapbox')
-
-m = m.build_map()
