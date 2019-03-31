43a4c813b8dc9727611eaae279a82f9a9cde48b8
Daniel Stone <danielstone411@gmail.com>
2019-03-31 20:51:19 +0100

    Updated comments

--- recolour.py
+++ recolour.py
@@ -2,22 +2,18 @@ import cv2
 import numpy
 
 def find_silver(image):
+    # Blur image to reduce noise
     image = cv2.GaussianBlur(image,(5,5),0)
+    # Get hsv and rgb channels
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     rgb = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
 
-    # cv2.imshow("HSV", hsv)
-    # cv2.waitKey();
+    # use HSV to remove dark areas
+    firstmask = cv2.inRange(hsv, (10,0,120), (85, 70, 255))
 
-    mask2 = cv2.inRange(hsv, (10,0,120), (85, 70, 255))
-    # mask2 = cv2.inRange(hsv, (0,0,180), (255, 255, 255))
+    #  Use RGB to mask buildings
+    newrgb = cv2.bitwise_and(rgb, rgb, mask=firstmask)
 
-    # cv2.imshow("first step", mask2)
-
-    newrgb = cv2.bitwise_and(rgb, rgb, mask=mask2)
     mask = cv2.inRange(newrgb, (150,150,140), (255, 255, 255))
-
-    # cv2.imshow("second step", mask)
-    # cv2.waitKey();
-
     return mask
+
