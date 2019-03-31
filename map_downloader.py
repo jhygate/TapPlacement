7ce4724c630cc8df6cb837809d176a4b1252d79a
Daniel Stone <danielstone411@gmail.com>
2019-03-31 12:22:27 +0100

    Display URL used to download map

--- map_downloader.py
+++ map_downloader.py
@@ -5,8 +5,9 @@ def download_patch(lat_long,api_key,zoom=19,dimensions=(1000,1000),file_format='
     x, y = dimensions
     URL = ('https://dev.virtualearth.net/REST/v1/Imagery/Map/Aerial/{},{}/'
             '{}?mapSize={},{}&format={}').format(latitude,longitude,zoom,x,y,file_format)
+    print("Downloading the map from {}".format(URL))
     image = requests.get(("{}&key={}").format(URL, api_key))
-    metadata = requests.get(("{}&mapMetadata=1&key={}").format(URL, api_key))
+    # metadata = requests.get(("{}&mapMetadata=1&key={}").format(URL, api_key))
 #     boundingbox = metadata.json()["resourceSets"][0]["resources"][0]["bbox"]
-#     print(boundingbox)
+    # print(metadata.text);
     return image
